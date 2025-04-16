730c2630264e28acdd0290eb297a84ac325f6de8
Somrat-Masud <somratmasud098@gmail.com>
2025-04-16 20:18:14 +0600

    module completed

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(express.json());
 app.use(cors());
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pnxdm.mongodb.net/?appName=Cluster0`;
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -31,8 +28,86 @@ async function run() {
 
     //jobs realated apis
     const jobsCollection = client.db("job-portal").collection("jobs");
+    const jobsApplicationsCollection = client
+      .db("job-portal")
+      .collection("job-applications");
+
+    // jobs related apis
     app.get("/jobs", async (req, res) => {
-      const result = await jobsCollection.find().toArray();
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = { hr_email: email };
+      }
+      const cursor = jobsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/jobs", async (req, res) => {
+      const newJob = req.body;
+      const result = await jobsCollection.insertOne(newJob);
+      res.send(result);
+    });
+
+    //jobs applications api
+    app.get("/job-application", async (req, res) => {
+      const email = req.query.email;
+      const query = { applicant_email: email };
+      const result = await jobsApplicationsCollection.find().toArray();
+
+      //fokira way to aggrigiate
+      for (const application of result) {
+        console.log(application.job_id);
+        const query_1 = { _id: new ObjectId(application.job_id) };
+        const job = await jobsCollection.findOne(query_1);
+        if (job) {
+          application.title = job.title;
+          application.location = job.location;
+          application.company = job.company;
+          application.company_logo = job.company_logo;
+        }
+      }
+      res.send(result);
+    });
+
+    // app.get('/job_applications/:id') ==>get a specification job application by id
+    app.get("/job-applications/jobs/:job_id", async (req, res) => {
+      const jobId = req.params.job_id;
+      const query = { job_id: jobId };
+      const result = await jobsApplicationsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/job-applications", async (req, res) => {
+      const application = req.body;
+      const result = await jobsApplicationsCollection.insertOne(application);
+      const id = application.job_id;
+      const query = { _id: new ObjectId(id) };
+      const job = await jobsCollection.findOne(query);
+      let newCount = 0;
+      if (job.applicationCount) {
+        newCount = job.applicationCount + 1;
+      } else {
+        newCount = 1;
+      }
+      //now updated info job
+      const filter = { _id: new ObjectId(id) };
+
+      const updatedDoc = {
+        $set: {
+          applicationCount: newCount,
+        },
+      };
+      const updatedResult = await jobsCollection.updateOne(filter, updatedDoc);
+      console.log(job);
       res.send(result);
     });
   } finally {
